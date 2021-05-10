2dbae73122c9e5790be176406a6e04e64fcbe4b1
yiaw <lsmin0703@gmail.com>
2021-05-10 23:54:45 +0900

    URL TREE 버그 발견 저장 코드

--- example/main.go
+++ example/main.go
@@ -6,25 +6,32 @@ import (
 )
 
 func main() {
-	utree := urltree.NewTree("User")
-	utree.Make("PUT", "/em-mmc/v1/command/dis-*")
-	utree.Make("PUT", "/em-mmc/v1/command/chg-sys-*")
+	utree := urltree.NewTree("auth")
+	utree.Make("GET", "/api/v1/user/:username")
+	utree.Make("POST", "/api/v1/user/:username")
+	utree.Make("DELETE", "/api/v1/user/:username")
+	utree.Make("PATCH", "/api/v1/user/:username")
+	utree.Make("GET", "/api/v1/user")
+	utree.Make("DELETE", "/api/v1/user")
+	utree.Make("GET", "/api/v1/config")
+	utree.Make("GET", "/api/v1/config/*")
 	utree.Printing()
 
-	var Method string = "PUT"
-	var inputURL = []string{
-		"/em-mmc/v1/command/dis-sys-rsc",
-		"/em-mmc/v1/command/dis-alm-info",
-		"/em-mmc/v1/command/chg-sys-info",
-		"/em-mmc/v1/command/chg-info-rsc",
-		"/em-mmc/v1/command/del-sys-rsc",
-		"/em-mmc/v1/command/chg-proc-rsc",
-		"/em-mmc/v1/command/del-sys-rsc",
-		"/em-mmc/v1/command/chg-proc-rsc"}
+	var Method = []string{"POST", "GET", "DELETE", "PATCH"}
 
-	for j := 0; j < len(inputURL); j++ {
-		ok, method, url := utree.MatchURL(Method, inputURL[j])
-		fmt.Printf("Bool = %-15t Method = %-7s URL = %s\n", ok, method, url)
+	var InputURL = []string{"/api/v1/config", "/api/v1/config/setting"}
+	/*
+		var InputURL = []string{"/api/v1/user",
+			"/api/v1/user/yiaw",
+			"/api/v1/alert/status",
+			"/api/v1/config",
+			"/api/v1/config/setting"}
+	*/
+	for i := 0; i < len(InputURL); i++ {
+		for j := 0; j < len(Method); j++ {
+			ok, method, url := utree.MatchURL(Method[j], InputURL[i])
+			fmt.Printf("Bool = %-15t Method = %-7s URL = %s\n", ok, method, url)
+		}
 	}
 
 }
